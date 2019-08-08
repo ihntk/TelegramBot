adc591b0b86f5d83ee182bf5b38459a7eb40af1d
Igor <tranzistor_2000@ukr.net>
2019-08-08 21:22:52 +0300

    Implemented downloadTorrent()

--- src/main/java/org/igor/telegram/TorrentDialog.java
+++ src/main/java/org/igor/telegram/TorrentDialog.java
@@ -1,7 +1,16 @@
 package org.igor.telegram;
 
+import org.telegram.telegrambots.meta.api.methods.GetFile;
 import org.telegram.telegrambots.meta.api.objects.Document;
+import org.telegram.telegrambots.meta.api.objects.File;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -12,6 +21,7 @@ public class TorrentDialog extends Dialog {
     private Path categoryPath;
     private Document document;
     private Properties properties;
+    private String homeDir = System.getProperty("user.home");
 
     private final String films;
     private final String mult;
@@ -39,35 +49,39 @@ public class TorrentDialog extends Dialog {
         for (String command : messageCommands) {
             switch (command) {
                 case ("/films"):
-                    categoryPath = Paths.get(films);
+                    categoryPath = Paths.get(homeDir, films);
                     break;
                 case ("/series"):
-                    categoryPath = Paths.get(series);
+                    categoryPath = Paths.get(homeDir, series);
                     break;
                 case ("/mult"):
-                    categoryPath = Paths.get(mult);
+                    categoryPath = Paths.get(homeDir, mult);
                     break;
                 case ("/packages"):
-                    categoryPath = Paths.get(packages);
+                    categoryPath = Paths.get(homeDir, packages);
                     break;
                 case ("/packagesWin"):
-                    categoryPath = Paths.get(packagesWin);
+                    categoryPath = Paths.get(homeDir, packagesWin);
                     break;
                 case ("/music"):
-                    categoryPath = Paths.get(music);
+                    categoryPath = Paths.get(homeDir, music);
                     break;
             }
         }
 
-        String fileName = document.getFileName();
-        boolean isTorrent = fileName.endsWith(".torrent");
-
+        String fileName;
+        boolean isTorrent = false;
+        if (document != null) {
+            fileName = document.getFileName();
+            isTorrent = fileName.endsWith(".torrent");
+        }
         if (categoryPath != null && isTorrent) {
             downloadTorrent();
             exitDialog();
             setAnswer("Torrent is downloading");
         } else if (categoryPath == null)
-            setAnswer("Select category");
+            setAnswer("Select category:\n" +
+                    "/films /mult /series /packages /packagesWin /music");
         else
             setAnswer("Input torrent file");
 
@@ -82,6 +96,18 @@ public class TorrentDialog extends Dialog {
     }
 
     private void downloadTorrent() {
-
+        try {
+            GetFile getFile = new GetFile();
+            getFile.setFileId(document.getFileId());
+            File file = Main.getBot().execute(getFile);
+            URL fileUrl = new URL(file.getFileUrl(Main.getBot().getBotToken()));
+            HttpURLConnection httpURLConnection = (HttpURLConnection) fileUrl.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            Files.copy(inputStream, Paths.get(categoryPath.toString(), document.getFileName()));
+        } catch (TelegramApiException | MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
